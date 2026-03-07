5bdfc71d302914441941d8f4fdb42d880fd478b3
Mathieu Legault <maty.legault@hotmail.com>
2026-03-07 12:01:37 -0500

    App init

---
+++ PromptCut/PromptCut/PromptCutApp.swift
@@ -0,0 +1,17 @@
+//
+//  PromptCutApp.swift
+//  PromptCut
+//
+//  Created by Mathieu Legault on 2026-03-07.
+//
+
+import SwiftUI
+
+@main
+struct PromptCutApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
